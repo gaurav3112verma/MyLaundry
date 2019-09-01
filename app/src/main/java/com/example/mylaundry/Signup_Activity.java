d0aa82acc147bb1c2e736dcb95bf6a869c5fe614
gaurav3112verma <38838963+gaurav3112verma@users.noreply.github.com>
2019-09-01 14:40:34 +0530

    initial commit

---
+++ app/src/main/java/com/example/mylaundry/Signup_Activity.java
@@ -0,0 +1,15 @@
+package com.example.mylaundry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle("Signup Form");
+
+    }
+}
